3cd8e8d898d995211c58d7e6a1372fd9a0515739
Nguyễn Anh Hào <nahhao74@gmail.com>
2026-09-07 17:21:09 +0700

    feat: export MVS-GI rig reference reader

--- nadir/data/__init__.py
+++ nadir/data/__init__.py
@@ -12,6 +12,7 @@ from .mvs_gi import (
     read_manifest_summary,
 )
 from .mvs_gi_pose import read_camera_image_poses
+from .mvs_gi_reference import MvsGiRigReference, read_mvs_gi_rig_reference
 from .mvs_gi_rig import build_mvs_gi_rig, camera_model_from_mvs_gi_spec
 
 __all__ = [
@@ -21,6 +22,7 @@ __all__ = [
     "MvsGiLayoutError",
     "MvsGiManifestSummary",
     "MvsGiSample",
+    "MvsGiRigReference",
     "decode_compressed_float_u8",
     "discover_csv_sources",
     "iter_mvs_gi_samples",
@@ -28,6 +30,7 @@ __all__ = [
     "read_compressed_float",
     "read_manifest_summary",
     "read_camera_image_poses",
+    "read_mvs_gi_rig_reference",
     "build_mvs_gi_rig",
     "camera_model_from_mvs_gi_spec",
 ]
