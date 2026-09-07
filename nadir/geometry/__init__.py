2a54089a369f2f12617b50fc6bca6e0c7af432b2
Nguyễn Anh Hào <nahhao74@gmail.com>
2026-09-07 16:11:10 +0700

    feat: export rig, LUT and observability geometry

--- nadir/geometry/__init__.py
+++ nadir/geometry/__init__.py
@@ -1,4 +1,17 @@
 from .double_sphere import DoubleSphereCamera
+from .lut import ProjectionLUT, build_projection_lut
+from .observability import PairObservability, compute_pair_observability
 from .rays import SphericalRayGrid, make_lower_hemisphere_grid
+from .rig import CameraRig, RigCamera
 
-__all__ = ["DoubleSphereCamera", "SphericalRayGrid", "make_lower_hemisphere_grid"]
+__all__ = [
+    "DoubleSphereCamera",
+    "ProjectionLUT",
+    "build_projection_lut",
+    "PairObservability",
+    "compute_pair_observability",
+    "SphericalRayGrid",
+    "make_lower_hemisphere_grid",
+    "CameraRig",
+    "RigCamera",
+]
