67f61c8a97750796bfb31a66c8d9f5533360ba4e
Nguyễn Anh Hào <nahhao74@gmail.com>
2026-09-07 16:43:55 +0700

    feat: export MVS-GI dataset adapter

--- nadir/data/__init__.py
+++ nadir/data/__init__.py
@@ -1,3 +1,24 @@
 from .manifest import ManifestDataset, NadirSample
+from .mvs_gi import (
+    MvsGiCsvSource,
+    MvsGiLayoutError,
+    MvsGiSample,
+    decode_compressed_float_u8,
+    discover_csv_sources,
+    iter_samples as iter_mvs_gi_samples,
+    load_samples as load_mvs_gi_samples,
+    read_compressed_float,
+)
 
-__all__ = ["ManifestDataset", "NadirSample"]
+__all__ = [
+    "ManifestDataset",
+    "NadirSample",
+    "MvsGiCsvSource",
+    "MvsGiLayoutError",
+    "MvsGiSample",
+    "decode_compressed_float_u8",
+    "discover_csv_sources",
+    "iter_mvs_gi_samples",
+    "load_mvs_gi_samples",
+    "read_compressed_float",
+]
