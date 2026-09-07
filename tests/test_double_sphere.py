c21e5524ea0788af53ed32a645cdaf22ac9b838a
Nguyễn Anh Hào <nahhao74@gmail.com>
2026-09-07 16:13:06 +0700

    test: cover >90-degree Double Sphere domain

--- tests/test_double_sphere.py
+++ tests/test_double_sphere.py
@@ -33,3 +33,25 @@ def test_pixel_roundtrip_in_valid_region():
     uv2, valid2 = cam.project(rays)
     assert np.all(valid2)
     np.testing.assert_allclose(uv2, uv, atol=1e-8)
+
+
+def test_projection_supports_valid_rays_beyond_90_degrees():
+    cam = camera()
+    theta = np.deg2rad(100.0)
+    ray = np.array([[np.sin(theta), 0.0, np.cos(theta)]])
+    uv, valid = cam.project(ray, check_bounds=False)
+    assert valid[0]
+    assert np.all(np.isfinite(uv[0]))
+
+    ray2, valid2 = cam.unproject(uv, check_bounds=False)
+    assert valid2[0]
+    np.testing.assert_allclose(ray2[0], ray[0], atol=1e-9)
+
+
+def test_projection_rejects_points_outside_double_sphere_domain():
+    cam = camera()
+    theta = np.deg2rad(170.0)
+    ray = np.array([[np.sin(theta), 0.0, np.cos(theta)]])
+    uv, valid = cam.project(ray, check_bounds=False)
+    assert not valid[0]
+    assert np.all(np.isnan(uv[0]))
