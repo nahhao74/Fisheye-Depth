b02a6bb54015e4c28de10e1b5667ec3257660b1c
Nguyễn Anh Hào <nahhao74@gmail.com>
2026-09-07 16:05:47 +0700

    feat: expose NADIR geometry primitives

---
+++ nadir/geometry/__init__.py
@@ -0,0 +1,4 @@
+from .double_sphere import DoubleSphereCamera
+from .rays import SphericalRayGrid, make_lower_hemisphere_grid
+
+__all__ = ["DoubleSphereCamera", "SphericalRayGrid", "make_lower_hemisphere_grid"]
