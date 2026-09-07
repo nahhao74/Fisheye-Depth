496fd561cfcfa5436c7d80bd6fff5d803ab3901a
Nguyễn Anh Hào <nahhao74@gmail.com>
2026-09-07 16:23:14 +0700

    feat: export depth candidate baselines

--- nadir/stereo/__init__.py
+++ nadir/stereo/__init__.py
@@ -1,3 +1,10 @@
+from .candidates import uniform_depth_candidates, uniform_inverse_depth_candidates
 from .photometric import PhotometricSweepResult, bilinear_sample, photometric_sphere_sweep
 
-__all__ = ["PhotometricSweepResult", "bilinear_sample", "photometric_sphere_sweep"]
+__all__ = [
+    "uniform_depth_candidates",
+    "uniform_inverse_depth_candidates",
+    "PhotometricSweepResult",
+    "bilinear_sample",
+    "photometric_sphere_sweep",
+]
