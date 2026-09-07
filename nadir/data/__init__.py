e167e3818c72efacde0b660a045412c45aa88b75
Nguyễn Anh Hào <nahhao74@gmail.com>
2026-09-07 16:48:47 +0700

    feat: export MVS-GI manifest inspection

--- nadir/data/__init__.py
+++ nadir/data/__init__.py
@@ -2,12 +2,14 @@ from .manifest import ManifestDataset, NadirSample
 from .mvs_gi import (
     MvsGiCsvSource,
     MvsGiLayoutError,
+    MvsGiManifestSummary,
     MvsGiSample,
     decode_compressed_float_u8,
     discover_csv_sources,
     iter_samples as iter_mvs_gi_samples,
     load_samples as load_mvs_gi_samples,
     read_compressed_float,
+    read_manifest_summary,
 )
 
 __all__ = [
@@ -15,10 +17,12 @@ __all__ = [
     "NadirSample",
     "MvsGiCsvSource",
     "MvsGiLayoutError",
+    "MvsGiManifestSummary",
     "MvsGiSample",
     "decode_compressed_float_u8",
     "discover_csv_sources",
     "iter_mvs_gi_samples",
     "load_mvs_gi_samples",
     "read_compressed_float",
+    "read_manifest_summary",
 ]
