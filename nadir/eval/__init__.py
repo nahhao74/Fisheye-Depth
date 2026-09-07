37eafdb13b6b92fb064811f6e7f1232cffccd0af
Nguyễn Anh Hào <nahhao74@gmail.com>
2026-09-07 17:20:50 +0700

    feat: export GT ray-grid reprojection

--- nadir/eval/__init__.py
+++ nadir/eval/__init__.py
@@ -1,3 +1,9 @@
 from .depth_metrics import DepthMetrics, compute_depth_metrics
+from .reproject_gt import RayGridGroundTruth, distance_image_to_ray_grid
 
-__all__ = ["DepthMetrics", "compute_depth_metrics"]
+__all__ = [
+    "DepthMetrics",
+    "compute_depth_metrics",
+    "RayGridGroundTruth",
+    "distance_image_to_ray_grid",
+]
