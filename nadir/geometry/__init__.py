368b9dc52641c989546d086433750526d5fede8f
Nguyễn Anh Hào <nahhao74@gmail.com>
2026-09-08 09:25:55 +0700

    feat: export Gate A triangulation primitives

--- nadir/geometry/__init__.py
+++ nadir/geometry/__init__.py
@@ -6,6 +6,12 @@ from .lut import ProjectionLUT, build_projection_lut
 from .observability import PairObservability, compute_pair_observability
 from .rays import SphericalRayGrid, make_lower_hemisphere_grid
 from .rig import CameraRig, RigCamera
+from .triangulation import (
+    TwoRayTriangulation,
+    array_pixels_to_body_rays,
+    project_body_points_to_array,
+    triangulate_two_rays,
+)
 
 __all__ = [
     "NativeCameraModel",
@@ -21,4 +27,8 @@ __all__ = [
     "make_lower_hemisphere_grid",
     "CameraRig",
     "RigCamera",
+    "TwoRayTriangulation",
+    "array_pixels_to_body_rays",
+    "project_body_points_to_array",
+    "triangulate_two_rays",
 ]
